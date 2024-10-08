84d9d59f9f179f2e9e533b345b7da6e8c48a045d
Jonathan King <jonathankingfc@gmail.com>
2024-10-08 17:43:06 -0400

    add metadata

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -1,15 +1,44 @@
-"use client";
-
+import { QCProvider } from "@/components/qc-provider";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import "../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const queryClient = new QueryClient({});
+export const metadata = {
+  description:
+    "Arches AI is the perfect tool to explore documents using artificial intelligence. Simply upload your PDF and start asking questions to your personalized chatbot.",
+  icons: {
+    icon: "/icon.png",
+  },
+  openGraph: {
+    description:
+      "Arches AI is the perfect tool to explore documents using artificial intelligence. Simply upload your PDF and start asking questions to your personalized chatbot.",
+    images: [
+      {
+        alt: "Arches AI",
+        height: 600,
+        url: "https://www.archesai.com/sc.png",
+        width: 800,
+      },
+    ],
+    title: "Arches AI",
+    type: "website",
+    url: "https://www.archesai.com/",
+  },
+  title: "Arches AI",
+  twitter: {
+    card: "summary_large_image",
+    description:
+      "Arches AI is the perfect tool to explore documents using artificial intelligence. Simply upload your PDF and start asking questions to your personalized chatbot.",
+    images: ["https://www.archesai.com/sc.png"],
+    title: "Arches AI",
+    url: "https://www.archesai.com/",
+  },
+} as Metadata;
 
 export default function RootLayout({
   children,
@@ -25,9 +54,7 @@ export default function RootLayout({
           disableTransitionOnChange
           enableSystem
         >
-          <QueryClientProvider client={queryClient}>
-            {children}
-          </QueryClientProvider>
+          <QCProvider>{children}</QCProvider>
           <Toaster />
         </ThemeProvider>
       </body>
