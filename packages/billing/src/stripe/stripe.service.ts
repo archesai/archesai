05e8fae8ab826be8c3ac9f30760ad95232b99ce9
Jonathan King <jonathankingfc@gmail.com>
2025-06-01 04:16:09 -0400

    fix: fix stripe version

--- packages/billing/src/stripe/stripe.service.ts
+++ packages/billing/src/stripe/stripe.service.ts
@@ -10,7 +10,7 @@ export class StripeService {
 
   constructor(configService: ConfigService) {
     this.stripe = new Stripe(configService.get('billing.stripe.token'), {
-      apiVersion: '2025-04-30.basil'
+      apiVersion: '2025-05-28.basil'
     })
   }
 
